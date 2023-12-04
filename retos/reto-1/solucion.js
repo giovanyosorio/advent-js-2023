2bb8031c399ba996e4e5d40bce4b8c0df62b1499
giovanyosorio <giovany.torres.799@gmail.com>
2023-12-03 19:19:04 -0500

    reto-1

---
+++ retos/reto-1/solucion.js
@@ -0,0 +1,24 @@
+function findFirstRepeated(gifts) {
+    const seen = new Set();
+    for (let id of gifts) {
+        if(seen.has(id)){
+
+          return id
+        }
+      seen.add(id);
+    }
+    return -1;
+}
+
+// Test cases
+const giftIds = [2, 1, 3, 5, 3, 2];
+const firstRepeatedId = findFirstRepeated(giftIds);
+console.log(firstRepeatedId); // 3
+
+const giftIds2 = [1, 2, 3, 4];
+const firstRepeatedId2 = findFirstRepeated(giftIds2);
+console.log(firstRepeatedId2); // -1
+
+const giftIds3 = [5, 1, 5, 1];
+const firstRepeatedId3 = findFirstRepeated(giftIds3);
+console.log(firstRepeatedId3); // 5
