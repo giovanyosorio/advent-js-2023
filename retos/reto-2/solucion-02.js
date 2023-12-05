1f776917581a64d18dc75dc2a06aa9fbea51355e
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-12-05 22:38:52 +0000

    reto-02

--- retos/reto-2/solucion-02.js
+++ retos/reto-2/solucion-02.js
@@ -1,4 +1,14 @@
-function manufacture(gifts, materials) {
-    // Code here
-    return []
-  }
+function manufacture(gifts, materials){
+  return gifts.filter(regalo=>{
+    return regalo.split("") // hola ["h","o",l,a]
+      .every(letra=>materials.includes(letra))
+    
+  })
+}
+
+
+// hacemos un filtro a tods los regalos
+//    itero las letras
+//      me fijo si esa letra esta en los materiales
+//         Cuando no hay una letra retorno false
+//         Si hay devolvemos true
