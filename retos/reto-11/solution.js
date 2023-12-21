e0b462687004c0cc99c52b83aa5cb837b2b890d1
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-12-21 20:03:21 +0000

    Reto 11

---
+++ retos/reto-11/solution.js
@@ -0,0 +1,22 @@
+function getIndexsForPalindrome(word) {
+
+  
+    const checkPalindrome=(word)=> word===word.split("").reverse().join("")
+    
+    const swap=(word,start,end) =>{
+    const palabra = word.split("");
+    [palabra[start],palabra[end]]= [palabra[end],palabra[start]];
+      return palabra.join("")
+      }
+    
+    if(checkPalindrome(word)) return []
+    
+  
+    for(let start=0;start<word.length;start++){
+     for(let end=0;end<word.length;end++){
+       if(checkPalindrome(swap(word,start,end)))return [start,end]
+     }
+    }
+    return null
+  }
+  
